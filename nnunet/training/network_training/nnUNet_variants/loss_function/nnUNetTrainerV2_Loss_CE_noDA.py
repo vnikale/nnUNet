25188d903678e66c78c2f01acb2068b47b27374a
Nikita Vladimirov <nikitav@mail.tau.ac.il>
2023-01-25 12:15:37 +0200

    fix CE_noDA trainer

--- nnunet/training/network_training/nnUNet_variants/loss_function/nnUNetTrainerV2_Loss_CE_noDA.py
+++ nnunet/training/network_training/nnUNet_variants/loss_function/nnUNetTrainerV2_Loss_CE_noDA.py
@@ -17,7 +17,7 @@ from nnunet.training.network_training.nnUNet_variants.data_augmentation.nnUNetTr
 
 
 
-class nnUNetTrainerV2_AL_Loss_CE_noDA(nnUNetTrainerV2_noDataAugmentation):
+class nnUNetTrainerV2_Loss_CE_noDA(nnUNetTrainerV2_noDataAugmentation):
     def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
                  unpack_data=True, deterministic=True, fp16=False):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
